c7c1ff9e20a811058164f7fa61d2a555ad64ab90
Ryszard Borzych <borzych@student.agh.edu.pl>
2022-06-04 17:54:46 +0200

    vis

--- main.m
+++ main.m
@@ -1,13 +1,17 @@
 clear;clc;
-limit=268;
+limit=343;
 data=table2array(readtable('Dane/Gotowe/set1.csv'));
 data2=table2array(readtable('Dane/Gotowe/set2.csv'));
 data=data(1:limit,3:7);
 data2=data2(1:limit,3:7);
 output=transpose(data(2:limit-1,1));
-data=[data(3:limit,2:5),data2(3:limit,:)];
+data=[data(3:limit,:),data2(3:limit,:)];
+% data=data(3:limit,:);
 input=transpose(data);
-net = feedforwardnet(10);
+% y=input(1,:);
+% histogram(input(7,:))56
+% plot(input(1,:), output, ".") 
+net = feedforwardnet([20,20]);
 net.divideParam.testRatio = 0.125;
 net.divideParam.valRatio = 0.125;
 net.trainParam.max_fail = 10;
